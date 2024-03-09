bb237d372332cb9086cc8ee09bca8316ac04075d
MRChiong <mark_chiong@rcc.edu.ph>
2024-03-09 15:41:53 +0800

    addition

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    
+    <form action="math.php" method="POST">
+Input1: <input type="text" name="Input1"><br>
+Input2: <input type="text" name="Input2"><br>
+<input type="submit">
+</form>
+   
+    <body>
+    </html>
---
+++ math.php
@@ -0,0 +1,8 @@
+<?php
+$Input1 = $_POST["Input1"];
+$Input2 = $_POST["Input2"];
+
+$output = $Input1+$Input2;
+
+echo "The sum is:" . $output;
+?>
